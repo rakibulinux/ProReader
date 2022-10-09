8c3945986b70581ec060f7874e2dfc607e33d9f4
Rakib Linux <rakibulinux@gmail.com>
2022-10-09 07:31:10 +0600

    Start now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-
+import React from "react";
+//My Name
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return <h1> Welcome to proReader</h1>;
 }
 
-export default App
+export default App;
